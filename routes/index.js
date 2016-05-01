ebce8c506708df3178b62b88cb14ecea8358bf0a
jedabero <jedabero@hotmail.com>
2016-04-30 19:20:03 -0500

    Added node-arff Index shows loaded arff file data Removed users route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
+var arff = require('node-arff');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  arff.load('storage/eucalyptus.arff', function (err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    //console.log(data.name);
+    //console.log(data.attributes);
+    //console.log(data.types);
+    //console.log(data.data);
+    var variables = {
+      title: 'Editor ARFF',
+      file: 'eucalyptus.arff',
+      dataset: {
+        name: data.name,
+        attributes: data.attributes,
+        types: data.types,
+        data: data.data
+      }
+    };
+    res.render('index', variables);
+  });
 });
 
 module.exports = router;
